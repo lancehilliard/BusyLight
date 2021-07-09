6df09aaaae8a0057bd7b7d43dbd8f2eb3bcdd093
Lance Hilliard <lance@digitalcreations.cc>
2021-07-09 17:55:35 -0500

    add draft version of Client project

---
+++ BusyLight.Client/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace BusyLight.Client {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.busyLightToolStripMenuItem = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.openStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "BusyLight";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.busyLightToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.openStripMenuItem,
+            this.quitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(241, 72);
+            // 
+            // busyLightToolStripMenuItem
+            // 
+            this.busyLightToolStripMenuItem.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.busyLightToolStripMenuItem.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.busyLightToolStripMenuItem.Enabled = false;
+            this.busyLightToolStripMenuItem.Name = "busyLightToolStripMenuItem";
+            this.busyLightToolStripMenuItem.Size = new System.Drawing.Size(180, 16);
+            this.busyLightToolStripMenuItem.Text = "BusyLight";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(237, 6);
+            // 
+            // openStripMenuItem
+            // 
+            this.openStripMenuItem.Name = "openStripMenuItem";
+            this.openStripMenuItem.Size = new System.Drawing.Size(240, 22);
+            this.openStripMenuItem.Text = "Open";
+            this.openStripMenuItem.Click += new System.EventHandler(this.openStripMenuItem_Click);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(240, 22);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.ToolTipText = "Quit BusyLight";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "BusyLight";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.contextMenuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox busyLightToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem openStripMenuItem;
+    }
+}
+
---
+++ BusyLight.Client/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.ComponentModel;
+using System.Configuration;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+using System.Windows.Forms;
+using BusyLight.Core;
+using RabbitMQ.Client;
+
+namespace BusyLight.Client {
+    public partial class Form1 : Form {
+        static readonly int PublishIntervalSeconds = Convert.ToInt32(ConfigurationManager.AppSettings["PublishIntervalSeconds"]);
+        static readonly IMicrophoneStatusChecker MicrophoneStatusChecker = new MicrophoneRegistryStatusChecker();
+        static readonly Core.ActivityPublisher ActivityPublisher = new Core.ActivityPublisher(new ConnectionFactory {Uri = new Uri(ConfigurationManager.AppSettings["MessageQueueUrl"])});
+        static readonly MicrophoneActivityPublisher MicrophoneActivityPublisher = new MicrophoneActivityPublisher(MicrophoneStatusChecker, ActivityPublisher);
+        readonly Thread _thread;
+
+        public Form1() {
+            InitializeComponent();
+            WindowState = FormWindowState.Minimized;
+            ShowInTaskbar = false;
+            _thread = new Thread(DoWork) {IsBackground = true};
+            _thread.Start();
+        }
+
+        protected override void OnClosing(CancelEventArgs e) {
+            WindowState = FormWindowState.Minimized;
+            e.Cancel = true;
+        }
+
+        [SuppressMessage("ReSharper", "FunctionNeverReturns", Justification = "Function runs in a background thread and is intended to run infinitely.")]
+        void DoWork() {
+            while (true) {
+                var secondsToWaitBeforeNextCheck = .25;
+                try {
+                    var activityPublished = MicrophoneActivityPublisher.PublishMicrophoneActivity();
+                    secondsToWaitBeforeNextCheck = activityPublished ? PublishIntervalSeconds : secondsToWaitBeforeNextCheck;
+                    notifyIcon1.Icon = activityPublished ? Properties.Resources.BusyLight_On : Properties.Resources.BusyLight_Off;
+                    notifyIcon1.Text = $@"BusyLight - {(activityPublished ? "On" : "Off")}";
+                }
+                catch (Exception e) {
+                    Console.WriteLine(e);
+                }
+                Thread.Sleep(TimeSpan.FromSeconds(secondsToWaitBeforeNextCheck));
+            }
+        }
+
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e) {
+            _thread.Abort();
+            Application.Exit();
+        }
+
+        private void openStripMenuItem_Click(object sender, EventArgs e) {
+            WindowState = FormWindowState.Normal;
+        }
+
+        void notifyIcon1_MouseClick(object sender, MouseEventArgs e) {
+            if (e.Button == MouseButtons.Left) {
+                WindowState = FormWindowState.Normal;
+            }
+        }
+    }
+}
